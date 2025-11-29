da77a11f70aa1fefd3844d9a496515d7ff21812b
shrobro1 <shrobro@gmail.com>
2025-11-29 20:48:29 +0000

    sign in, edit and delete control, and open ai api interaction. no json yet

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,28 @@
 class RecipesController < ApplicationController
 
-  def home
-    render({ :template => "recipe_templates/home" })
-  end
+def home
+  require "openai"
+
+  #client = OpenAI::Client.new
+
+  #begin
+    #completion = client.chat.completions.create(
+      #model: :"gpt-4o-mini",
+      #messages: [
+        #{ role: "user", content: "Reply with exactly: API OK" }
+      #]
+    #)
+
+   # @openai_test = completion.choices.first.message.content
+  #rescue => e
+   # @openai_test = "Error talking to OpenAI: #{e.class} - #{e.message}"
+  #end
+
+  render template: "recipe_templates/home"
+end
+
+
+
 
 
   def index
@@ -30,12 +50,12 @@ class RecipesController < ApplicationController
     the_recipe.ingredients = params.fetch("query_ingredients")
     the_recipe.instructions = params.fetch("query_instructions")
     the_recipe.servings = params.fetch("query_servings")
-    the_recipe.creator_id = params.fetch("query_creator_id")
+    the_recipe.creator_id = current_user.id
     the_recipe.source_url = params.fetch("query_source_url")
 
     if the_recipe.valid?
       the_recipe.save
-      redirect_to("/recipes", { :notice => "Recipe created successfully." })
+      redirect_to("/recipes/#{the_recipe.id}", { :notice => "Recipe created successfully." })
     else
       redirect_to("/recipes", { :alert => the_recipe.errors.full_messages.to_sentence })
     end
@@ -69,4 +89,27 @@ class RecipesController < ApplicationController
 
     redirect_to("/recipes", { :notice => "Recipe deleted successfully." } )
   end
+
+  def user_prompt
+     user_input = params[:query_prompt]
+
+      # Initialize OpenAI client
+      client = OpenAI::Client.new
+
+     begin
+       completion = client.chat.completions.create(
+         model: "gpt-4o-mini",
+         messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: user_input }
+         ]
+        )
+
+       @answer = completion.choices.first.message.content
+     rescue => e
+       @answer = "Error talking to OpenAI: #{e.class} - #{e.message}"
+     end
+      render({ :template => "recipe_templates/response" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   #------------------------------
 
+  post("/user_prompt", {:controller => "recipes", :action => "user_prompt"})
+
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 end
